07e122f9097a3e66aba7f123603f9c944aeca8e0
Ngniguepa Therence <ngniguepafaha@gmail.com>
2024-01-26 04:10:01 +0100

    lastest one

--- src/pages/google/index.js
+++
@@ -1,9 +0,0 @@
-import passport from "passport";
-import "../../../lib/passport";
-
-export default async function (req, res, next) {
-  passport.authenticate("google", {
-    scope: ["profile", "email"],
-    session: false,
-  })(req, res, next);
-}
